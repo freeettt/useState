8bfd4f6efa66b5df31a03a77b6c16facec81bee0
freeettt <122921308+freeettt@users.noreply.github.com>
2025-05-05 13:04:43 +0300

    Update and rename src to App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserForm from './components/UserForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Реєстраційна форма</h1>
+      <UserForm />
+    </div>
+  );
+};
+
+export default App;
